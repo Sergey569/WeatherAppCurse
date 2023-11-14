03fb335619fed215a39a9f9bef8e76d753ee5565
Sergey D <dryahlov98@mail.ru>
2023-11-14 23:47:49 +0300

    Создал проект

---
+++ app/src/main/java/com/example/weatherappcurse/DayItem.kt
@@ -0,0 +1,11 @@
+package com.example.weatherappcurse
+
+data class DayItem(
+    val citi: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val hours: String
+)
